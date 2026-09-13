41b4460853e11757e2f3a85be9a608f11819a0a8
SaboraFruta <saborafruta7@gmail.com>
2026-09-13 15:27:52 -0300

    fix: reconciliar migracoes core da analise

---
+++ apps/core/migrations/0069_merge_analise_custo_compra.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0063_analise_custo_compra'),
+        ('core', '0068_parametrossistema_checkout_venda'),
+    ]
+
+    operations = []
